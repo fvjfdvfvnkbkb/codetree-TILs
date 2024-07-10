9a50ced14230946548517ba973e6ab673d307c8d
euihwan <167052973+fvjfdvfvnkbkb@users.noreply.github.com>
2024-07-10 11:39:18 +0900

    Title: 비교에 따른 연산, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240710/비교에 따른 연산/operation-based-on-comparison.py
+++ 240710/비교에 따른 연산/operation-based-on-comparison.py
@@ -1,9 +1,10 @@
-# 두 정수를 입력받습니다
-a = 10
-b = 5
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
 
 # 조건에 따라 결과를 출력합니다
 if a > b:
     print(a * b)
-else:
+elif a < b:
     print(b // a)
